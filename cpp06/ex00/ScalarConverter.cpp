c5c10a1e461cfe5b1b29621779f0ae0e1e377fd3
le_krabbe <krabbelars95@gmail.com>
2023-05-14 21:58:55 +0200

    cpp6

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -3,9 +3,20 @@
 #include	<iostream>
 #include	"ScalarConverter.hpp"
 
+void	ScalarConverter::convert_to_d(std::string arg)
+{
+	char** endptr;
+	double tmp = strtod(arg.c_str(),endptr);
+	if (endptr == '\0')
+	{
+		_d = tmp;
+		std::cout << _d << std::endl;
+	}
+
+}
+
+
 void	ScalarConverter::convert(std::string arg)
 {
-	std::stoi(arg);
-	std::stof(arg);
-	std::stod(arg);
+	convert_to_d(arg);
 }
--- cpp06/ex00/ScalarConverter.hpp
+++ cpp06/ex00/ScalarConverter.hpp
@@ -1,8 +1,50 @@
 
 #include	<string>
 
+
+#ifndef CLASS_ScalarConverter
+# define CLASS_ScalarConverter
+
 class ScalarConverter
 {
+	private:
+		ScalarConverter();
+		ScalarConverter(const ScalarConverter &a);
+		~ScalarConverter();
+		ScalarConverter &operator= (const ScalarConverter& a);
+		void static convert_to_d(std::string arg);
 	public:
-	void	static convert(std::string arg);
-};
+		static int		errors;
+		static char		_c;
+		static int		_i;
+		static float	_f;
+		static double	_d;
+		void			static convert(std::string arg);};
+
+#endif
+
+/*
+
+#include	"ScalarConverter.hpp"
+
+ScalarConverter::ScalarConverter()
+{
+	
+}
+
+ScalarConverter::ScalarConverter(const ScalarConverter &a)
+{
+	
+}
+
+ScalarConverter::~ScalarConverter()
+{
+	
+}
+
+ScalarConverter& ScalarConverter::operator= (const ScalarConverter& a)
+{
+	
+	return (*(this));
+}
+*/
