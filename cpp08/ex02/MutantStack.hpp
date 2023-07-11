1dde76075240531c36c85f75298b3e8433d79718
larskrabbe <92089860+larskrabbe@users.noreply.github.com>
2023-07-11 15:48:18 +0200

    where is 9

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -1,1 +1,41 @@
-#ifndef
+#ifndef CLASS_MUTANTSTACK
+# define CLASS_MUTANTSTACK
+
+// #include	<stack>
+#include	<deque>
+
+template<typename type, typename container = std::deque<type> >
+class MutantStack : public  std::stack<type, container>
+{
+public:
+	MutantStack(/* args */) : std::stack<type,container>(){};
+	MutantStack(const MutantStack& src) : std::stack<type,container>(src){};
+	~MutantStack();
+	MutantStack& operator=(const MutantStack& src)
+	{
+        if (this == &src)
+            return (*this);
+        std::stack<type,container>::operator=(src);
+        return (*this);	
+	};
+	typedef typename container::iterator			iterator;
+	typedef typename container::reverse_iterator	reverse_iterator;
+	iterator	begin()
+	{
+		return (MutantStack::stack::c.begin());
+	};
+	iterator	end()
+	{
+		return (MutantStack::stack::c.end());
+	};
+	reverse_iterator	rbegin()
+	{
+		return (MutantStack::stack::c.rbegin());
+	};
+	reverse_iterator	rend()
+	{
+		return (MutantStack::stack::c.rend());
+	};
+};
+
+#endif
